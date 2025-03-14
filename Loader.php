19c3e0977eb54b0945f69705b7e7891d195293f0
Иван Трутнев <trutnev@intervolga.ru>
2025-03-14 18:40:04 +0300

    Убрал логи, оставил автоматическое подключение только php файлов

--- Loader.php
+++ Loader.php
@@ -6,22 +6,16 @@ class Loader
 	{
 		$files = scandir($dir);
 
-		file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log/dir.log', __FILE__ . ':' . __LINE__ . "\n(" . date('Y-m-d H:i:s').")\n" . print_r($files, TRUE) . "\n\n");
-
 		foreach ($files as $file) {
 
-			file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log/file.log', __FILE__ . ':' . __LINE__ . "\n(" . date('Y-m-d H:i:s').")\n" . print_r($file, TRUE) . "\n\n", FILE_APPEND);
-
-			if ($file == "." || $file == ".."){
+			if ($file == "." || $file == ".." || is_file($file) && pathinfo($file, PATHINFO_EXTENSION) != "php"){
 				continue;
 			}
 
 			if (is_file($dir.DIRECTORY_SEPARATOR.$file)) {
-				file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log/__debug.log', __FILE__ . ':' . __LINE__ . "\n(" . date('Y-m-d H:i:s').")\n" . print_r($file, TRUE) . "\n\n", FILE_APPEND);
 				require_once $dir . DIRECTORY_SEPARATOR . $file;
 			}
 			elseif (is_dir($dir.DIRECTORY_SEPARATOR.$file)){
-				file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log/__debug.log', __FILE__ . ':' . __LINE__ . "\n(" . date('Y-m-d H:i:s').")\n" . print_r($file, TRUE) . "\n\n", FILE_APPEND);
 				static::includeFiles($dir . DIRECTORY_SEPARATOR . $file);
 			}
 		}
